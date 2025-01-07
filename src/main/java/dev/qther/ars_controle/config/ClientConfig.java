32a7d321e7216d1152d819214da7851b55e449e1
Qther <qther@tuta.io>
2025-01-07 20:36:09 +0800

    feat: remove spellbook control improvements as they are now in Ars Nouveau itself

--- src/main/java/dev/qther/ars_controle/config/ClientConfig.java
+++ src/main/java/dev/qther/ars_controle/config/ClientConfig.java
@@ -3,28 +3,8 @@ package dev.qther.ars_controle.config;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class ClientConfig {
-    public final ModConfigSpec.BooleanValue AUTOFOCUS_SEARCH;
-    public final ModConfigSpec.BooleanValue CLEAR_SEARCH_ON_AUTOFOCUS;
-    public final ModConfigSpec.BooleanValue ESCAPE_TO_CLEAR_SEARCH;
-
-    public final ModConfigSpec.BooleanValue SWAP_GLYPHS_WITH_NUMBER_KEYS;
 
     ClientConfig(ModConfigSpec.Builder builder) {
-        var improvedControls = new ConfigHelper.CategoryBuilder(builder, "enhanced_spellbook_controls");
-        improvedControls.push("Config for Enhanced Spell Book controls");
-
-        var searchBar = improvedControls.child("search_bar");
-        searchBar.push("Search bar tweaks");
-        AUTOFOCUS_SEARCH = searchBar.bool("enable_auto_focus", false, "Enable auto-focus");
-        CLEAR_SEARCH_ON_AUTOFOCUS = searchBar.bool("clear_on_auto_focus", true, "Clear search on auto-focus");
-        ESCAPE_TO_CLEAR_SEARCH = searchBar.bool("escape_to_clear", false, "Escape to clear search");
-        searchBar.pop();
-
-        var spellCrafting = improvedControls.child("spell_crafting");
-        spellCrafting.push("Spell crafting tweaks");
-        SWAP_GLYPHS_WITH_NUMBER_KEYS = spellCrafting.bool("swap_glyphs_with_number_keys", false, "Swap glyphs with number keys");
-
-        improvedControls.pop();
     }
 
     public static final ClientConfig CLIENT;
