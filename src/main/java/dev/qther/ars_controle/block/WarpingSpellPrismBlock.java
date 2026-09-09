a4a91a3c0adc67b36ff514a1e4fdcd877bde4770
Qther <qther@tuta.io>
2026-09-09 20:25:40 +0800

    fix: warping spell prism using incorrect spell direction when directly hitting a block (#23)

--- src/main/java/dev/qther/ars_controle/block/WarpingSpellPrismBlock.java
+++ src/main/java/dev/qther/ars_controle/block/WarpingSpellPrismBlock.java
@@ -14,6 +14,7 @@ import dev.qther.ars_controle.block.tile.WarpingSpellPrismTile;
 import dev.qther.ars_controle.config.ACServerConfig;
 import dev.qther.ars_controle.packets.clientbound.PacketRenderBlockOutline;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
@@ -160,7 +161,7 @@ public class WarpingSpellPrismBlock extends ModBlock implements IPrismaticBlock,
                 }
             }
 
-            hit = b.withDirection(spell.getDirection());
+            hit = b.withDirection(Direction.getNearest(spell.getDeltaMovement()));
         }
 
         if (hit instanceof EntityHitResult e) {
